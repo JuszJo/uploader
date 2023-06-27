acf9bc8bb6528509a919d117422f3df0c8df9d39
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-06-27 22:29:51 +0100

    add see all function from db

--- controllers/storage.controller.js
+++ controllers/storage.controller.js
@@ -11,10 +11,12 @@ async function handleUpload(req, res) {
     req.on('end', () => {
         const singleBuffer = Buffer.concat(chunks)
 
+        const base64 = singleBuffer.toString('base64')
+
         UserModel.findOneAndUpdate({name: req.headers.user}, {$push: {
             files: {
                 name: req.headers.name,
-                file: singleBuffer
+                file: base64
             }
         }})
         .then(() => {
@@ -45,59 +47,113 @@ function handleDownload(req, res) {
     })
 }
 
-function handleSeeAll(req, res) {
-    fs.readdir('./images', async (err, files) => {
+async function handleSeeAll(req, res) {
+    try {
+        const user = await UserModel.findOne({name: req.session.user})
+
+        const files = user.files
+
         if(files.length == 0) res.send("No Files")
         else {
             let images = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <style>
-                    div {
-                        display: grid;
-                        grid-template: repeat(4, 300px) / repeat(3, 300px);
-                        justify-content: center;
-                        gap: 20px;
-                        margin: 0 50px;
-                    }
-            
-                    img {
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                    }
-            
-            
-                </style>
-            </head>
-            <body>
-                <div>
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                    <style>
+                        div {
+                            display: grid;
+                            grid-template: repeat(4, 300px) / repeat(3, 300px);
+                            justify-content: center;
+                            gap: 20px;
+                            margin: 0 50px;
+                        }
+                
+                        img {
+                            width: 100%;
+                            height: 100%;
+                            object-fit: cover;
+                        }
+                
+                
+                    </style>
+                </head>
+                <body>
+                    <div>
             `
     
-            const arrayOfImages = await Promise.all(files.map(async file => {
-                const buffer = await fs.promises.readFile(`./images/${file}`)
+            for(let i = 0; i < files.length; ++i) {
+                const file = files[i].file
     
-                const base64 = buffer.toString('base64')
+                const html = `<img src='data:/image/jpeg;base64,${file}' />`
     
-                const html = `<img src='data:/image/jpeg;base64,${base64}' />`
-    
-                images += html;
-    
-                return html
-            }))
+                images += html
+            }    
     
             res.send(images + `</div></body></html>`)
         }
-    })
+    }
+    catch(err) {
+        if(err) throw err
+    }
 }
+
+// function handleSeeAll(req, res) {
+//     fs.readdir('./images', async (err, files) => {
+//         if(files.length == 0) res.send("No Files")
+//         else {
+//             let images = `
+//             <!DOCTYPE html>
+//             <html lang="en">
+//             <head>
+//                 <meta charset="UTF-8">
+//                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//                 <title>Document</title>
+//                 <style>
+//                     div {
+//                         display: grid;
+//                         grid-template: repeat(4, 300px) / repeat(3, 300px);
+//                         justify-content: center;
+//                         gap: 20px;
+//                         margin: 0 50px;
+//                     }
+            
+//                     img {
+//                         width: 100%;
+//                         height: 100%;
+//                         object-fit: cover;
+//                     }
+            
+            
+//                 </style>
+//             </head>
+//             <body>
+//                 <div>
+//             `
+    
+//             const arrayOfImages = await Promise.all(files.map(async file => {
+//                 const buffer = await fs.promises.readFile(`./images/${file}`)
+    
+//                 const base64 = buffer.toString('base64')
+    
+//                 const html = `<img src='data:/image/jpeg;base64,${base64}' />`
+    
+//                 images += html;
+    
+//                 return html
+//             }))
+    
+//             res.send(images + `</div></body></html>`)
+//         }
+//     })
+// }
 
 module.exports = {
     handleUpload,
     handleSeeAll,
-    handleDownload
+    handleDownload,
 }
